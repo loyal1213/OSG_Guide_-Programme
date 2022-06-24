96d71d5a48a8ca2cf42b3d7b39e7a27210fc21e0
loyal1213 <zhongwenjiaqwer@163.com>
2022-06-24 19:20:18 +0800

    add 第五讲 基本图元 上

--- application/application.cpp
+++ application/application.cpp
@@ -4,11 +4,12 @@
 #include "stdafx.h"
 
 #include "第3讲 osgviewer 应用基础 01.hpp"
-
+#include "第5讲 基本图元(上).hpp"
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// 第3讲 osgviewer 应用基础 01
-	test_osgviewer();
+	// test_osgviewer();
+	TestGeode();
 	return 0;
 }
 
