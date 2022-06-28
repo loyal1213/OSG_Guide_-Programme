ace81b644644e0ed6df6358bb130e7206e682180
loyal1213 <zhongwenjiaqwer@163.com>
2022-06-28 22:36:27 +0800

    修改忽略文件

--- application/application.cpp
+++ application/application.cpp
@@ -5,11 +5,15 @@
 
 #include "第3讲 osgviewer 应用基础 01.hpp"
 #include "第5讲 基本图元(上).hpp"
+#include "第6讲 基本图元(下).hpp"
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// 第3讲 osgviewer 应用基础 01
 	// test_osgviewer();
-	TestGeode();
+	// TestGeode();
+	// 第6讲 基本图元下
+	TestGeom();
+
 	return 0;
 }
 
