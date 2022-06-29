0be1149857c111f87157eab9b2f5b4ab4bd289aa
zhongwenjia <zhongwenjiaqwer@163.com>
2022-06-29 13:04:23 +0800

    Add files via upload

---
+++ application/第7讲 矩阵操作(上).hpp
@@ -0,0 +1,49 @@
+#include <osg/Group>
+#include <osg/Node>
+#include <osg/Geode>
+#include <osg/Geometry>
+
+#include <osgViewer/Viewer>
+
+#include <osgDB/ReadFile>
+#include <osgDB/ReaderWriter>
+
+#include <osg/MatrixTransform>
+#include <osg/AnimationPath>
+
+osg::ref_ptr<osg::Node> MatrixOperation(){
+
+	osg::ref_ptr<osg::Group> group = new osg::Group();
+	osg::ref_ptr<osg::MatrixTransform> matrix_transform = new osg::MatrixTransform();
+	osg::ref_ptr<osg::Node> node = osgDB::readNodeFile("glider.osg");
+
+
+	matrix_transform->addChild(osgDB::readNodeFile("glider.osg"));
+	matrix_transform->setMatrix(osg::Matrix::translate(5.0,0.0,0.0));
+	matrix_transform->setUpdateCallback(new osg::AnimationPathCallback(osg::Vec3(0.0, 2.0 ,0.0), osg::Y_AXIS, 1.0));
+
+
+	group->addChild(osgDB::readNodeFile("glider.osg"));
+	group->addChild(matrix_transform);
+	group->addChild(node.get());
+
+	return group;
+
+}
+
+
+
+
+void TestMatrix(){
+
+	osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer();
+
+	osg::ref_ptr<osg::Group> group = new osg::Group();
+
+
+
+	viewer->setSceneData(MatrixOperation().get());
+
+	viewer->run();
+
+}
